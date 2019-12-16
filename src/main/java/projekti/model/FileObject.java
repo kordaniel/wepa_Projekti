894c95e6d8f93c6bc41003fa59ec868d80cbc5d8
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-12-16 11:10:46 +0200

    heroku

--- src/main/java/projekti/model/FileObject.java
+++ src/main/java/projekti/model/FileObject.java
@@ -1,26 +1,21 @@
 package projekti.model;
 
 import java.time.LocalDateTime;
-import javax.persistence.Basic;
+//import javax.persistence.Basic;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Lob;
-//import javax.validation.constraints.NotEmpty;
+//import javax.persistence.FetchType;
+//import javax.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-//import org.hibernate.annotations.Type;
+import org.hibernate.annotations.Type;
 import org.hibernate.annotations.UpdateTimestamp;
-//import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor
-//@Profile("default")
 public class FileObject extends AbstractPersistable<Long> {
-    // @NotEmpty annotation in this class results in application
-    // crashing. Atleast with H2-database.
     
     @CreationTimestamp
     private LocalDateTime createDateTime;
@@ -28,19 +23,15 @@ public class FileObject extends AbstractPersistable<Long> {
     @UpdateTimestamp
     private LocalDateTime updateDateTime;
     
-    //@NotEmpty
     private String name;
     
-    //@NotEmpty
     private String contentType;
     
-    //@NotEmpty
     private Long size;
     
-    //@NotEmpty
-    @Lob
-    @Basic(fetch = FetchType.LAZY)
-    //@Type(type = "org.hibernate.type.BinaryType") //heroku postgresql
+    //@Lob
+    //@Basic(fetch = FetchType.LAZY)
+    @Type(type = "org.hibernate.type.BinaryType") //heroku postgresql
     private byte[] content;
     
 }
